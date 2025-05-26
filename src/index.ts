7f7776bbe6ca2ea0727f5f4218a7e9dbdb8bb8d5
Jesus <jadrianlg16@gmail.com>
2025-05-26 12:54:45 -0600

    port fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import taskRoutes from './routes/taskRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
